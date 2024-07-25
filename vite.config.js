db85764dc1c11e13d3211561bbb42d3b2fa2e9de
Ashish Armarkar <85281482+Ashish-Armarkar@users.noreply.github.com>
2024-07-25 15:33:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/365BuildBlogs/',  // Add this line
+  base: 'https://ashish-armarkar.github.io/365BuildBlogs/',  // Add this line
   plugins: [react()],
 })
